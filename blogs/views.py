d8e0a15b6c506030a403ab172cfc3cbbec4e0f6e
jcastillo2011 <alejandrocastillo2011@gmail.com>
2025-09-20 16:26:42 -0400

    listado de post en el template

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView  #vistas genericas
+
+#la vista es donde se relacionada el template con el modelo
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post  #nombre del modelo donde estan los datos
+    template_name = 'post_list.html' #nombre del template a usar que esta en la carpeta templates
+    
